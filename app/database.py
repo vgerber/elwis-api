0830c968c9edc2492f0af87ba16526f1b0d11654
vgerber <vg-development@web.de>
2025-08-16 18:22:36 +0200

    feat: Added sqlite database option

--- app/database.py
+++ app/database.py
@@ -3,17 +3,25 @@ from typing import Annotated
 from fastapi import Depends
 from sqlmodel import SQLModel, Session, create_engine
 
+DB_TYPE = os.getenv("DB_TYPE", "postgres")
 
-# Read Postgres connection info from environment variables
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-
-postgres_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
-engine = create_engine(postgres_url, echo=False)
+if DB_TYPE == "sqlite":
+    SQLITE_PATH = os.getenv("SQLITE_PATH", "sqlite.db")
+    db_url = f"sqlite:///{SQLITE_PATH}"
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(db_url, echo=False, connect_args=connect_args)
+elif DB_TYPE == "postgres":
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+    db_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    engine = create_engine(db_url, echo=False)
+else:
+    raise ValueError(
+        f"Unsupported DB_TYPE: {DB_TYPE}. Supported types are 'sqlite' and 'postgres'."
+    )
 
 
 def create_db_and_tables():
